56acdacc725f823aeb69309d7422314e0c5fd7c3
TranMinhTamttttt <tamtran11037469@gmail.com>
2025-09-26 14:10:12 +0700

    fix

--- AttendanceSystem/Services/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace AttendanceSystem.Services
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string toEmail, string subject, string htmlContent, string attachmentPath = null);
-    }
-}
